d9f5509fa351d770d4bfc0c860df7dda6cfa08e4
edenn001 <edenn001.fiu.edu>
2014-02-02 13:04:41 -0500

    search bar at top of app added

---
+++ src/com/example/fishselector/Fishes.java
@@ -0,0 +1,22 @@
+package com.example.fishselector;
+
+import java.util.HashMap;
+
+public class Fishes {
+
+
+
+		   HashMap<String,String> fishes = new HashMap<String, String>(){{
+		    put("BARRAMUNDI","test"); put("ALASKA COD","test"); put("ALBACORE","test"); put("ARCTIC CHAR","test"); put("ATLANTIC CROAKER","test"); put("ATLANTIC MACKEREL","test"); put("ATLANTIC POLLOCK","test"); 
+		    put("BAY SCALLOPS","test"); put("BLACK SEA BASS","test");  put("BLOOD COCKLE","test"); put("BLUE MUSSEL","test"); put("BLUE MUSSEL","test"); 
+		    put("BUTTER CLAM","test"); put("CAPELIN, SMELT ROE","test"); put("CARIBBEAN SPINY LOBSTER","test");  put("CATFISH","test"); put("CRAWFISH","test"); put("DUNGENESS CRAB","test");
+		    put("EASTERN OR AMERICAN OYSTER","test"); put("EDIBLE OYSTER","test"); put("LONGFIN SQUID","test"); put("MAHIMAHI","test"); put("MANILA CLAM","test"); put("NEW ZEALAND GREEN MUSSEL","test");
+		    put("OYSTERS (FARMED)","test"); put("OYSTERS (WILD)","test"); put("PACIFIC GEODUCK (FARMED)","test"); put("PACIFIC HALIBUT","test"); put("PACIFIC LITTLENECK","test"); put("PACIFIC OYSTER","test");
+		    put("PACIFIC SARDINES","test"); put("PINK SHRIMP","test"); put("RAINBOW TROUT","test"); put("SABLEFISH/BLACK COD","test"); put("SEA URCHIN(CANADA)","test"); put("SNOW CRAB(US)","test");
+		    put("SOFTSHELL CLAM","test"); put("SPINY LOBSTER","test"); put("SPOT PRAWN","test"); put("SPOTTED/SILVER SEATROUT (LA, FL)","test"); put("STONE CRAB","test"); put("STRIPED BASS (FARMED)","test");
+		    put("STRIPED MULLET","test"); put("TILAPIA (ECUADOR)","test"); put("TILAPIA(U.S.)","test"); put("U.S. HADDOCK (HOOK AND LINE)","test"); put("WEATHERVANE SCALLOP","test"); put("WILD ALASKAN SALMON","test");
+		    put("WRECKFISH","test"); put("YELLOW PERCH (LAKE ERIE)","test"); put("YELLOWFIN (U.S. ATLANTIC TROLL/POLE)","test"); put("U.S. HADDOCK (HOOK AND LINE)","test"); put("WEATHERVANE SCALLOP","test"); put("WILD ALASKAN SALMON","test");}};
+
+	
+	
+}
